686aa66efa966fc15216f196b424dcedf3618024
Rafael Pacheco <ravpacheco@gmail.com>
2017-10-01 10:30:43 -0300

    Initial commit

---
+++ src/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Take.Blip.ContentTypesHelper
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/UnitTests/Take.Blip.ContentTypesHelper.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Take.Blip.ContentTypesHelper.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
